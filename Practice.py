bf6ba592d56706614d47e55d3f1e4bb3e3afa6fd
nlima21 <110039165+nlima21@users.noreply.github.com>
2023-11-19 17:42:13 -0500

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -12,5 +12,5 @@ st.subheader("Personal info")
 first_name = st.text_input('first name')
 last_name = st.text_input('Last Name')
 
-major = st.selectbox('What is your major, 
+major = st.selectbox('What is your major', 
                      ["","CS", "IT", "Math", "DS"])
