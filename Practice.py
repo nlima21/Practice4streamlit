74502ec4b0aee72a007fdb7a54a38f0c1013a598
nlima21 <110039165+nlima21@users.noreply.github.com>
2023-11-19 17:39:32 -0500

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,1 +1,16 @@
-print("Hello, World")
+import streamlit as st
+
+
+
+st.title("Practice Streamlit")
+st.header("Ilove MAth")
+
+st.subheader("Personal info")
+
+
+
+first_name = st.text_input('first name')
+last_name = st.text_input('Last Name')
+
+major = st.selectbox('What is your major, 
+                     ["","CS", "IT", "Math", "DS"])
